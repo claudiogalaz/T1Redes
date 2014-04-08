c342fe32abbc59ffa62880c9c94dbdc9aa2937a5
ManuFigueroa <manuel.figueroa@alumnos.usm.cl>
2014-04-07 23:55:52 -0300

    Revert "Revert "máspruebas""
    This reverts commit 290af302a871cb0c5ac15741339f7e8effbb67d0.

--- src/tarea/SimpleHttpServer.java
+++ src/tarea/SimpleHttpServer.java
@@ -13,19 +13,20 @@ import com.sun.net.httpserver.HttpServer;
 public class SimpleHttpServer {
 
   public static void main(String[] args) throws Exception {
-    HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-    server.createContext("/test", new MyHandler());
+    HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+    server.createContext("/", new Handler1());
     server.setExecutor(null); // creates a default executor
     server.start();
   }
 
-  static class MyHandler implements HttpHandler {
+  static class Handler1 implements HttpHandler {
     public void handle(HttpExchange t) throws IOException {
-      String response = "Welcome Real's HowTo test page";
+      String response = "Primera prueba";
       t.sendResponseHeaders(200, response.length());
       OutputStream os = t.getResponseBody();
       os.write(response.getBytes());
       os.close();
     }
   }
+
 }
